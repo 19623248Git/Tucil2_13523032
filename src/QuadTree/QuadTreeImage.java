82373eda35a119a8fd46e0e13dcd910627015be4
19623248Git <19623248@std.stei.itb.ac.id>
2025-03-30 02:07:06 +0700

    feat: :bricks: overhaul objects and structure of codebase

--- src/QuadTree/QuadTreeImage.java
+++ src/QuadTree/QuadTreeImage.java
@@ -4,20 +4,12 @@ import java.io.IOException;
 
 class QuadTreeImage extends ImageProcessing{
 
-        // Misc attributes
-        private Pixel meanPixel;
-
         // Error measurement attributes
-        // Storing the value of RGB not each color channel
-        private double variance;
+        // Storing the threshold value of RGB not each color channel
         private double var_thres;
-        private double mad;
         private double mad_thres;
-        private double mpd;
         private double mpd_thres;
-        private double entropy;
         private double entr_thres;
-        private double ssim;
         private double ssim_thres;
 
         // Algorithm attributes
@@ -26,7 +18,7 @@ class QuadTreeImage extends ImageProcessing{
         private double compressPercent;
 
         // Node structure
-        public static class Node{
+        public class Node{
 
                 // Data attributes
                 private int start_x;
@@ -34,25 +26,32 @@ class QuadTreeImage extends ImageProcessing{
                 private int start_y;
                 private int end_y;
                 private double error;
+                private Pixel meanPixel;
 
                 // Tree data structure
                 // private Node parent; // maybe a quad tree doesn't need a parent node, let's remove it now
                 private Node ne, se, sw, nw; // instead of list of childrens, determine 4 childrens
+
+                // Get mean of pixels between x1 - x2 and y1 - y2
+                public void meanPixelRange(){
+                        this.meanPixel.setRGB(0, 0, 0);
+                        double n = (this.end_x - this.start_x + 1) * (this.end_y - this.start_y + 1);
+                        for (int i = this.start_y; i <= this.end_y; i++) {
+                                for (int j = this.start_x; j <= this.end_x; j++){ // (i , j) -> (row, col)
+                                        this.meanPixel.add(getPixelValue(j, i));
+                                }
+                        }
+                        this.meanPixel.divMean(n);
+                }
         }
         
 
         public QuadTreeImage(){
                 super();
-                this.meanPixel = new Pixel();
-                this.variance = 0;
                 this.var_thres = 0;
-                this.mad = 0;
                 this.mad_thres = 0;
-                this.mpd = 0;
                 this.mpd_thres = 0;
-                this.entropy = 0;
                 this.entr_thres = 0;
-                this.ssim = 0;
                 this.ssim_thres = 0;
                 this.minBlockSize = 4;
                 this.compressPercent = 0;
@@ -61,16 +60,10 @@ class QuadTreeImage extends ImageProcessing{
 
         public QuadTreeImage(String absPath) throws IOException{
                 super(absPath);
-                this.meanPixel = new Pixel();
-                this.variance = 0;
                 this.var_thres = 0;
-                this.mad = 0;
                 this.mad_thres = 0;
-                this.mpd = 0;
                 this.mpd_thres = 0;
-                this.entropy = 0;
                 this.entr_thres = 0;
-                this.ssim = 0;
                 this.ssim_thres = 0;
                 this.minBlockSize = 4;
                 this.compressPercent = 0;
@@ -78,35 +71,17 @@ class QuadTreeImage extends ImageProcessing{
         }
 
         // Getters
-        public Pixel getMeanPixel() { 
-                return meanPixel; 
-        }
-        public double getVariance() { 
-                return variance; 
-        }
         public double getVarThres() { 
                 return var_thres; 
-        }
-        public double getMad() { 
-                return mad; 
         }
         public double getMadThres() { 
                 return mad_thres; 
         }
-        public double getMpd() { 
-                return mpd; 
-        }
         public double getMpdThres() { 
                 return mpd_thres; 
         }
-        public double getEntropy() { 
-                return entropy; 
-        }
         public double getEntrThres() { 
                 return entr_thres; 
-        }
-        public double getSsim() { 
-                return ssim; 
         }
         public double getSsimThres() { 
                 return ssim_thres; 
@@ -122,35 +97,17 @@ class QuadTreeImage extends ImageProcessing{
         }
 
         // Setters
-        public void setMeanPixel(Pixel meanPixel) { 
-                this.meanPixel = meanPixel; 
-        }
-        public void setVariance(double variance) { 
-                this.variance = variance; 
-        }
         public void setVarThres(double var_thres) { 
                 this.var_thres = var_thres; 
-        }
-        public void setMad(double mad) { 
-                this.mad = mad; 
         }
         public void setMadThres(double mad_thres) { 
                 this.mad_thres = mad_thres; 
         }
-        public void setMpd(double mpd) { 
-                this.mpd = mpd; 
-        }
         public void setMpdThres(double mpd_thres) { 
                 this.mpd_thres = mpd_thres; 
         }
-        public void setEntropy(double entropy) { 
-                this.entropy = entropy; 
-        }
         public void setEntrThres(double entr_thres) { 
                 this.entr_thres = entr_thres; 
-        }
-        public void setSsim(double ssim) { 
-                this.ssim = ssim; 
         }
         public void setSsimThres(double ssim_thres) { 
                 this.ssim_thres = ssim_thres; 
@@ -165,63 +122,49 @@ class QuadTreeImage extends ImageProcessing{
                 this.compressPercent = compressPercent; 
         }
 
-        // Get mean of pixels between x1 - x2 and y1 - y2
-        public void meanPixelRange(int x_start, int x_end, int y_start, int y_end){
-                this.meanPixel.setRGB(0, 0, 0);
-                double n = (x_end - x_start + 1) * (y_end - y_start + 1);
-                for (int i = y_start; i < y_end; i++) {
-                        for (int j = x_start; j < x_end; j++){ // (i , j) -> (row, col)
-                                this.meanPixel.add(getPixelValue(j, i));
-                        }
-                }
-                this.meanPixel.divMean(n);
-        }
-
-        public void calcVariance(int x_start, int x_end, int y_start, int y_end){
+        public double calcVariance(int x_start, int x_end, int y_start, int y_end, Pixel meanPixel){
                 double var_r = 0;
                 double var_g = 0;
                 double var_b = 0;
                 double n = (x_end - x_start + 1) * (y_end - y_start + 1);
-                meanPixelRange(x_start, x_end, y_start, y_end);
-                for(int i = y_start; i < y_end; i++){
-                        for(int j = x_start; j < x_end; j++){ // (i , j) -> (row, col)
-                                var_r+=Math.pow(getPixelValue(j, i).getR()-this.meanPixel.getR(), 2);
-                                var_g+=Math.pow(getPixelValue(j, i).getG()-this.meanPixel.getG(), 2);
-                                var_b+=Math.pow(getPixelValue(j, i).getB()-this.meanPixel.getB(), 2);
+                for(int i = y_start; i <= y_end; i++){
+                        for(int j = x_start; j <= x_end; j++){ // (i , j) -> (row, col)
+                                var_r+=Math.pow(getPixelValue(j, i).getR()-meanPixel.getR(), 2);
+                                var_g+=Math.pow(getPixelValue(j, i).getG()-meanPixel.getG(), 2);
+                                var_b+=Math.pow(getPixelValue(j, i).getB()-meanPixel.getB(), 2);
                         }
                 }
                 var_r/=n;
                 var_g/=n;
                 var_b/=n;
-                this.variance = (var_r + var_g + var_b) / 3;
+                return ((var_r + var_g + var_b) / 3);
         }
 
-        public void calcMad(int x_start, int x_end, int y_start, int y_end) {
+        public double calcMad(int x_start, int x_end, int y_start, int y_end, Pixel meanPixel) {
                 double mad_r = 0;
                 double mad_g = 0;
                 double mad_b = 0;
                 double n = (x_end - x_start + 1) * (y_end - y_start + 1);
-                meanPixelRange(x_start, x_end, y_start, y_end);
-                for (int i = y_start; i < y_end; i++) {
-                        for (int j = x_start; j < x_end; j++) { // (i , j) -> (row, col)
-                                mad_r += Math.abs(getPixelValue(j, i).getR() - this.meanPixel.getR());
-                                mad_g += Math.abs(getPixelValue(j, i).getG() - this.meanPixel.getG());
-                                mad_b += Math.abs(getPixelValue(j, i).getB() - this.meanPixel.getB());
+                for (int i = y_start; i <= y_end; i++) {
+                        for (int j = x_start; j <= x_end; j++) { // (i , j) -> (row, col)
+                                mad_r += Math.abs(getPixelValue(j, i).getR() - meanPixel.getR());
+                                mad_g += Math.abs(getPixelValue(j, i).getG() - meanPixel.getG());
+                                mad_b += Math.abs(getPixelValue(j, i).getB() - meanPixel.getB());
                         }
                 }
                 
                 mad_r /= n;
                 mad_g /= n;
                 mad_b /= n;
-                this.mad = (mad_r + mad_g + mad_b) / 3;
+                return ((mad_r + mad_g + mad_b) / 3);
         }
         
-        public void calcMpd(int x_start, int x_end, int y_start, int y_end){
+        public double calcMpd(int x_start, int x_end, int y_start, int y_end){
                 double max_r = 0;
                 double max_g = 0;
                 double max_b = 0;
-                for (int i = y_start; i < y_end; i++) {
-                        for (int j = x_start; j < x_end; j++) { // (i , j) -> (row, col)
+                for (int i = y_start; i <= y_end; i++) {
+                        for (int j = x_start; j <= x_end; j++) { // (i , j) -> (row, col)
                                 if(max_r < getPixelValue(j, i).getR()){
                                         max_r = getPixelValue(j, i).getR();
                                 }
@@ -233,15 +176,15 @@ class QuadTreeImage extends ImageProcessing{
                                 }
                         }
                 }
-                this.mpd = (max_r + max_g + max_b) / 3;
+                return ((max_r + max_g + max_b) / 3);
         }
 
-        public void calcEntropy(int x_start, int x_end, int y_start, int y_end){
+        public double calcEntropy(int x_start, int x_end, int y_start, int y_end){
                 double entr_r = 0;
                 double entr_g = 0;
                 double entr_b = 0;
-                for (int i = y_start; i < y_end; i++) {
-                        for (int j = x_start; j < x_end; j++) { // (i , j) -> (row, col)
+                for (int i = y_start; i <= y_end; i++) {
+                        for (int j = x_start; j <= x_end; j++) { // (i , j) -> (row, col)
                                 if(entr_r == 0){entr_r = 0;}
                                 if(entr_g == 0){entr_g = 0;}
                                 if(entr_b == 0){entr_b = 0;}
@@ -253,7 +196,7 @@ class QuadTreeImage extends ImageProcessing{
                 entr_r*=-1;
                 entr_b*=-1;
                 entr_g*=-1;
-                this.entropy = (entr_r + entr_g + entr_b) / 3;
+                return ((entr_r + entr_g + entr_b) / 3);
         }
 
 }
