81e201614cd2acf36baa4f8191135d19055d146f
19623248Git <19623248@std.stei.itb.ac.id>
2025-03-30 16:42:08 +0700

    feat: :sparkles: Divide and Conquer Algorithm

--- src/QuadTree/QuadTreeImage.java
+++ src/QuadTree/QuadTreeImage.java
@@ -16,6 +16,7 @@ class QuadTreeImage extends ImageProcessing{
         private int mode;
         private int minBlockSize;
         private double compressPercent;
+        private Node root;
 
         // Node structure
         public class Node{
@@ -25,6 +26,7 @@ class QuadTreeImage extends ImageProcessing{
                 private int end_x;
                 private int start_y;
                 private int end_y;
+                private int size;
                 private double error;
                 private Pixel meanPixel;
 
@@ -32,13 +34,110 @@ class QuadTreeImage extends ImageProcessing{
                 // private Node parent; // maybe a quad tree doesn't need a parent node, let's remove it now
                 private Node ne, se, sw, nw; // instead of list of childrens, determine 4 childrens
 
+                // default constructor
                 public Node(){
                         this.start_x = 0;
-                        this.end_x = getWidth();
+                        this.end_x = getWidth()-1;
                         this.start_y = 0;
-                        this.end_y = getHeight();
+                        this.end_y = getHeight()-1;
+                        this.error = 0;
+                        this.size = (this.end_x - this.start_x + 1) * (this.end_y - start_y + 1);
+                        this.meanPixel = new Pixel();
                 }
 
+                // user defined constructor
+                public Node(int start_x, int end_x, int start_y, int end_y){
+                        this.start_x = start_x;
+                        this.end_x = end_x;
+                        this.start_y = start_y;
+                        this.end_y = end_y;
+                        this.error = 0;
+                        this.size = (this.end_x - this.start_x + 1) * (this.end_y - start_y + 1);
+                        this.meanPixel = new Pixel();
+                }
+
+                // Getter
+                public int getStartX() {
+                        return start_x;
+                }
+        
+                public int getEndX() {
+                        return end_x;
+                }
+        
+                public int getStartY() {
+                        return start_y;
+                }
+        
+                public int getEndY() {
+                        return end_y;
+                }
+
+                public int getSize(){
+                        return size;
+                }
+        
+                public double getError() {
+                        return error;
+                }
+        
+                public Pixel getMeanPixel() {
+                        return meanPixel;
+                }
+
+                // Setter
+                public void setStartX(int start_x) {
+                        this.start_x = start_x;
+                }
+        
+                public void setEndX(int end_x) {
+                        this.end_x = end_x;
+                }
+        
+                public void setStartY(int start_y) {
+                        this.start_y = start_y;
+                }
+        
+                public void setEndY(int end_y) {
+                        this.end_y = end_y;
+                }
+
+                public void setSize(int size){
+                        this.size = size;
+                }
+        
+                public void setError(double error) {
+                        this.error = error;
+                }
+        
+                public void setMeanPixel(Pixel meanPixel) {
+                        this.meanPixel = meanPixel;
+                }
+
+                // Helper methods
+                public boolean isLeaf() {
+                        return(ne == null && se == null && sw == null && nw == null);
+                }
+
+                // Divide and Conquer
+                public void DnC(){
+                        computeError(this);
+                        if(!errorThresholdCheck(this) && !(this.size <= getMinBlockSize())){
+                                int start_dividedX = (this.end_x - this.start_x + 1)/2;
+                                int start_dividedY = (this.end_y - this.start_y + 1)/2;
+                                this.ne = new Node(start_dividedX, this.end_x, this.start_y ,start_dividedY-1);
+                                this.se = new Node(start_dividedX, this.end_x, start_dividedY, this.end_y);
+                                this.sw = new Node(this.start_x, start_dividedX-1, start_dividedY, this.end_y);
+                                this.nw = new Node(this.start_x, start_dividedX-1, this.start_y ,start_dividedY-1);
+                                ne.DnC();
+                                se.DnC();
+                                sw.DnC();
+                                nw.DnC();
+                        }
+                        else{
+                                // do nothing
+                        }
+                }
 
         }
         
@@ -53,6 +152,7 @@ class QuadTreeImage extends ImageProcessing{
                 this.minBlockSize = 4;
                 this.compressPercent = 0;
                 this.mode = 0; // default mode
+                this.root = new Node();
         }
 
         public QuadTreeImage(String absPath) throws IOException{
@@ -65,6 +165,7 @@ class QuadTreeImage extends ImageProcessing{
                 this.minBlockSize = 4;
                 this.compressPercent = 0;
                 this.mode = 0; // default mode
+                this.root = new Node();
         }
 
         // Getters
@@ -121,7 +222,7 @@ class QuadTreeImage extends ImageProcessing{
 
         // Get mean of pixels between x1 - x2 and y1 - y2
         public Pixel meanPixelRange(int start_x, int end_x, int start_y, int end_y){
-                Pixel meanPixel = new Pixel(0,0,0);
+                Pixel meanPixel = new Pixel();
                 double n = (end_x - start_x + 1) * (end_y - start_y + 1);
                 for (int i = start_y; i <= end_y; i++) {
                         for (int j = start_x; j <= end_x; j++){ // (i , j) -> (row, col)
@@ -133,17 +234,23 @@ class QuadTreeImage extends ImageProcessing{
         }
 
         public double computeError(Node node){
+                node.meanPixel = meanPixelRange(node.start_x, node.end_x, node.start_y, node.end_y);
                 return switch (this.mode) {
-                        case 0 -> {
-                                node.meanPixel = meanPixelRange(node.start_x, node.end_x, node.start_y, node.end_y);
-                                yield calcVariance(node.start_x, node.end_x, node.start_y, node.end_y, node.meanPixel);
-                        }
-                        case 1 -> {
-                                node.meanPixel = meanPixelRange(node.start_x, node.end_x, node.start_y, node.end_y);
-                                yield calcMad(node.start_x, node.end_x, node.start_y, node.end_y, node.meanPixel);
-                        }
+                        case 0 -> calcVariance(node.start_x, node.end_x, node.start_y, node.end_y, node.meanPixel);
+                        case 1 -> calcMad(node.start_x, node.end_x, node.start_y, node.end_y, node.meanPixel);
                         case 2 -> calcMpd(node.start_x, node.end_x, node.start_y, node.end_y);
                         case 3 -> calcEntropy(node.start_x, node.end_x, node.start_y, node.end_y);
+                        default -> throw new IllegalArgumentException("Invalid mode: " + this.mode);
+                }; 
+        }
+
+        // if error is less than threshold return true
+        public boolean errorThresholdCheck(Node node){
+                return switch (this.mode) {
+                        case 0 -> node.error <= this.var_thres;
+                        case 1 -> node.error <= this.mad_thres;
+                        case 2 -> node.error <= this.mpd_thres;
+                        case 3 -> node.error <= this.entr_thres;
                         default -> throw new IllegalArgumentException("Invalid mode: " + this.mode);
                 }; 
         }
@@ -223,6 +330,10 @@ class QuadTreeImage extends ImageProcessing{
                 entr_b*=-1;
                 entr_g*=-1;
                 return ((entr_r + entr_g + entr_b) / 3);
+        }
+
+        public void compress(){
+                root.DnC();
         }
 
 }
